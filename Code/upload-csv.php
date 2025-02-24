c1fa3c62f3bdc549c3fc5f3519e21308407f4097
ThaoVy413 <vyb2203487@student.ctu.edu.vn>
2025-02-24 22:34:57 +0700

    Add files via upload

---
+++ Code/upload-csv.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+<form action="upload-csv-processing.php" method="post" enctype="multipart/form-data">
+<label for="csvfile">Choose CSV File:</label>
+<br>
+<input type="file" name="csvfile" id="csvfile">
+
+<input type="submit" value="Upload CSV" name="submit">
+</form>
+</body>
+</html>
---
+++ Code/upload-img.php
@@ -0,0 +1,23 @@
+<!-- <!DOCTYPE html>
+<html>
+<body>
+
+<form action="upload.php" method="post" enctype="multipart/form-data">
+Ban chon 1 anh de lam profile:
+  <input type="file" name="fileToUpload" id="fileToUpload">
+  <input type="submit" value="Upload Image" name="submit">
+</form>
+
+</body>
+</html> -->
+<!DOCTYPE html>
+<html>
+<body>
+<form action="upload-csdl.php" method="post" enctype="multipart/form-data">
+Ban chon 1 anh de lam profile:
+<input type="file" name="fileToUpload" id="fileToUpload">
+
+<input type="submit" value="Upload Image" name="submit">
+</form>
+</body>
+</html>
